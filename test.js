6567c1cb71d2d36c8c1ca000e1c7c42c75f26171
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-01-09 14:39:40 +0100

    Fix tests

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ var wktParser = require('./wkt.build.js');
 var fixtures = require('./test-fixtures.json');
 
 fixtures.forEach((item, i)=>{
-  test(`fixture ${i}`, t=>{
+  test(`fixture ${i + 1}`, t=>{
     var out = wktParser(item.code);
-    //console.log(JSON.stringify(out, false, 2));
-    const diff = JSON.stringify(compare(item.value, out), null, 2);
+    //console.log(JSON.stringify(out, null, 2));
+    const diff = JSON.stringify(compare(item.value, JSON.parse(JSON.stringify(out))), null, 2);
     t.equal(diff, '[]');
     t.end();
   });
